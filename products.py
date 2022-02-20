f4dcee7e843afe2a2fe9f74b10a1337353de2126
Dennis Chiang <dennis8654@gmail.com>
2022-02-20 20:15:16 +0800

    insert defined functions

--- products.py
+++ products.py
@@ -1,53 +1,54 @@
 # account book application
 
 import os
-products = []
-
-if os.path.isfile('products.csv'): # Check if the file is the folder
-	print('The file has been found !!!')
-	# read past saved csv file
-	with open('products.csv', 'r') as f:
-		for line in f:
-			if 'product, price' in line:
-				continue
-			name, price = line.strip().split(',')
-			products.append([name, price])
-	print(products)
-else:
-	print('The file cannot be found...')
 
 # read past saved csv file
-products = []
-with open('products.csv', 'r') as f:
-	for line in f:
-		if 'product, price' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+def read_file(filename):
+	products = []
+	if os.path.isfile(filename): # Check if the file is the folder
+		print('The file has been found !!!')
+		# read past saved csv file
+		with open(filename, 'r') as f:
+			for line in f:
+				if 'product, price' in line:
+					continue
+				name, price = line.strip().split(',')
+				products.append([name, price])
+		print(products)
+	else:
+		print('The file cannot be found...')
+	return products
 
 # input new products & price you buy
-while True:
-	name = input('Please enter the name of the product: ')
-	if name == 'q':
-		break
-	price = input('How much is this product?: ')
-	price = int(price)
-	p = []
-	p.append(name)
-	p.append(price)
-	products.append(p)
-	# Quicker way: p = [name, price]
-	# Even quicker: products.append([name, price])
-print(products)
+def user_input(products):
+	while True:
+		name = input('Please enter the name of the product: ')
+		if name == 'q':
+			break
+		price = input('How much is this product?: ')
+		price = int(price)
+		p = []
+		p.append(name)
+		p.append(price)
+		products.append(p)
+		# Quicker way: p = [name, price]
+		# Even quicker: products.append([name, price])
+	print(products)
+	return products
 
 # List out all purchase history
-for product in products:
-	print('the', product[0], 'has cost you', str(product[1]))
+def print_products(products):
+	for product in products:
+		print('the', product[0], 'has cost you', str(product[1]))
 
 # Write into file
-with open('products.csv', 'w') as f:
-	f.write('product, price\n')
-	for x in products:
-		f.write(x[0] + ',' + str(x[1]) + '\n')
+def write_file(filename, products):
+	with open(filename, 'w') as f:
+		f.write('product, price\n')
+		for x in products:
+			f.write(x[0] + ',' + str(x[1]) + '\n')
 
+products = read_file('products.csv')
+products = user_input(products)
+print_products(products)
+write_file('products.csv', products)
